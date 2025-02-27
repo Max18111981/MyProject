33ff2a2e103e669b49e45fdbae1d5892a80eb967
Max18111981 <maksplastinin81@gmail.com>
2025-02-27 21:02:56 +0700

    27.02.2025г

--- hairdresser_service/courses/static/js/scripts.js
+++ hairdresser_service/courses/static/js/scripts.js
@@ -42,14 +42,18 @@ document.addEventListener('copy', function(event) {
 
 
 let duration = 0.8;
-let delay = 0.3;
-let revealText = document.querySelector(".reveal");
-let letters = revealText.textContent.split("");
-revealText.textContent = "";
-let middle = letters.filter(e => e !== " ").length / 2;
-letters.forEach((letter, i) => {
-  let span = document.createElement("span");
-  span.textContent = letter;
-  span.style.animationDelay = `${delay + Math.abs(i - middle) * 0.1}s`;
-  revealText.append(span);
-});
+let delay = 0;
+let reveals = document.querySelectorAll(".reveal");
+
+reveals.forEach((reveal) => {
+    let letters = reveal.textContent.split("");
+    reveal.textContent = "";
+    let middle = letters.filter(e => e !== " ").length / 2;
+    letters.forEach((letter, i) => {
+      let span = document.createElement("span");
+      span.textContent = letter;
+      span.style.animationDelay = `${delay + Math.abs(i - middle) * 0.1}s`;
+      reveal.append(span);
+    });
+})
+
